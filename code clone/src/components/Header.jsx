c545c53e1f900d0f5bf0d3ce2548feed2544dc6e
patitapaban <patitapabanpanda046@gmail.com>
2024-05-09 12:04:50 +0530

    resopnsive code pen

--- code clone/src/components/Header.jsx
+++ code clone/src/components/Header.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import { AppBar, Toolbar, styled } from "@mui/material";
+
 const Container = styled(AppBar)`
   background: black;
   height: 9vh;
+
+  /* For small screens (phones, 600px and below) */
+  @media only screen and (max-width: 600px) {
+    height: 8vh; /* Adjust height for small screens */
+  }
 `;
 
 const Header = () => {
